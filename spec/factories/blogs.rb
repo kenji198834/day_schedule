76b21ad37104d74d90feaf9c92ff53f95afe9397
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-02 15:23:16 +0900

    afterメソッドを用いて、生成するダミーデータに画像を添付

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :blog do
     title {Faker::Lorem.sentence}
+  
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  
   end
 end
